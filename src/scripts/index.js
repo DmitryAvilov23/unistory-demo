9320015e1655d0f1f309293371fa57f09080b6d2
DmitryAvilov23 <86045965+DmitryAvilov23@users.noreply.github.com>
2021-08-06 19:06:40 +0300

    create second block

---
+++ src/scripts/index.js
@@ -0,0 +1,15 @@
+import "../babel";
+import "../sass/style.sass";
+// Картинки
+import Sprite from "../img/sprite.svg";
+import HeroBanner from "../img/hero/hero-banner.svg";
+import Upwork from "../img/hero/upwork.svg";
+import Step1 from "../img/steps/step1.svg";
+import Step2 from "../img/steps/step2.svg";
+import Step3 from "../img/steps/step3.svg";
+import Step4 from "../img/steps/step4.svg";
+import StepBg from "../img/steps/step-bg.svg";
+// Модули
+import test from "./modules/test";
+
+test();
